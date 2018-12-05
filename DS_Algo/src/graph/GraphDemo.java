0ea15309b5f4ab3ad6e236bb78608b04d7c98a4e
kamal <kamal@192.168.0.4>
2018-12-05 04:22:25 -0500

    BFS and DFS added

--- DS_Algo/src/graph/GraphDemo.java
+++ DS_Algo/src/graph/GraphDemo.java
@@ -11,6 +11,80 @@ public class GraphDemo {
 	}
 	
 	/*
+	 * Utility method to perform the dfs recursively
+	 */
+	void dfsUtil(int source, Vector<Integer> visited) {
+		visited.add(source);
+		System.out.print(source + " ");
+		
+		Iterator<Integer> it = graph.adjList[source].iterator();
+		
+		while (it.hasNext()) {
+			int p = it.next();
+			
+			if (!visited.contains(p))
+				dfsUtil(p, visited);
+		}
+	}
+	
+	/*
+	 * Depth first traversal
+	 */
+	void dfs(int source) {
+		
+		Vector<Integer> visited = new Vector<Integer> ();
+		System.out.println("*** dfs traversal ***");
+		dfsUtil(source, visited);
+	
+	}
+	
+	/*
+	 * Depth first traversal for disconnected graph
+	 */
+	void dfsDisconnected() {
+		
+		Vector<Integer> visited = new Vector<Integer> ();
+		System.out.println("*** dfs traversal ***");
+
+		//Check all the vertex
+		for (int i = 0; i < graph.vertex; i++) {
+			if (!visited.contains(i))
+				dfsUtil(i, visited);
+		}
+
+	}
+	
+	/*
+	 * Breadth first traversal using Queue
+	 */
+	void bfs(int source) {
+		LinkedList<Integer> queue = new LinkedList<Integer> ();
+		Vector<Integer> visited = new Vector<Integer> ();
+		
+		queue.add(source);
+		visited.add(source);
+		
+		System.out.println("*** bfs traversal ***");
+		
+		// until queue is empty
+		while (queue.size() > 0) {
+			int vertex = queue.poll();
+			System.out.print(vertex + " ");
+			
+			Iterator<Integer> it = graph.adjList[vertex].iterator();
+			
+			while (it.hasNext()) {
+				int p = it.next();
+				
+				if (!visited.contains(p)) {
+					queue.add(p);
+					visited.add(p);
+				}
+			}
+		}
+	}
+	
+	/*
 	 * add edge between source and destination vertex (Directed graph)
 	 */
 	void addEdgeDirected(int s, int d) {
@@ -36,12 +110,15 @@ public class GraphDemo {
 		
 		System.out.println("*** Graph ***");
 		for (int i = 0; i < graph.vertex; i++) {
+			System.out.print("Vertex " + i + " -> ");
 			@SuppressWarnings("rawtypes")
 			Iterator iterator = graph.adjList[i].iterator();
 			while (iterator.hasNext())
 				System.out.print(iterator.next() + " ");
+			
+			System.out.println("");
 		}
-		System.out.println("");
+		System.out.println("** Display Graph Done ***");
 		
 	}
 	
--- DS_Algo/src/graph/Startup.java
+++ DS_Algo/src/graph/Startup.java
@@ -4,16 +4,17 @@ public class Startup {
 
 	public static void main(String[] args) {
 		
-		GraphDemo graphDemo = new GraphDemo(5);
+		GraphDemo graphDemo = new GraphDemo(4);
 		graphDemo.addEdgeDirected(0, 1);
-		graphDemo.addEdgeDirected(1, 4);
-		graphDemo.addEdgeDirected(1, 3);
-		graphDemo.addEdgeDirected(2, 1);
-		graphDemo.addEdgeDirected(3, 2);
-		graphDemo.addEdgeDirected(4, 0);
-		graphDemo.addEdgeDirected(4, 3);
+		graphDemo.addEdgeDirected(1, 2);
+		graphDemo.addEdgeDirected(0, 2);
+		graphDemo.addEdgeDirected(2, 0);
+		graphDemo.addEdgeDirected(2, 3);
+		graphDemo.addEdgeDirected(3, 3);
 
 		graphDemo.printGraph();
+//		graphDemo.bfs(0);
+		graphDemo.dfs(2);
 		
 	}
 
