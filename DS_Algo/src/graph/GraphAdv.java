0b63bb03033244cf7de928b60d4274b4464754ba
kamal <dbkamal@gmail.com>
2018-12-15 01:35:46 -0500

    Modified Graph class

--- DS_Algo/src/graph/GraphAdv.java
+++ DS_Algo/src/graph/GraphAdv.java
@@ -5,55 +5,78 @@ import java.util.*;
 /*
  * This class separates vertex and edges and treated them as class
  */
-public class GraphAdv<T> {
-	@SuppressWarnings("unused")
-	private List<Edge<T>> allEdges;
+public class GraphAdv<E> {
+	LinkedList<Vertex<E>>[] adjList;
+	@SuppressWarnings("rawtypes")
+	LinkedList<Edge> edgeList;
+	int vertex;
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	GraphAdv(int v) {
+		vertex = v;
+		edgeList = new LinkedList<Edge> ();
+		adjList = new LinkedList[v];
+
+		for (int i = 0; i < vertex; i++) {
+			adjList[i] = new LinkedList<Vertex<E>> ();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	void addEdgeUndirectedWeight(int v1, int v2, int weight) {
+		Vertex<Integer> v11 = new Vertex<Integer> (v1);
+		Vertex<Integer> v22 = new Vertex<Integer> (v2);
+		@SuppressWarnings("rawtypes")
+		Edge edge = new Edge(v11, v22, weight);
+		edgeList.add(edge);
+		adjList[v1].add((Vertex<E>) v22);
+		adjList[v2].add((Vertex<E>) v11);
+	}
 }
 
-class Vertex<T> {
-	
-}
+class Vertex<E> {
+	private int k;
+	private int val;
 
-class Edge<T> {
-	private boolean isDirected = false;
-	private Vertex<T> vertex1;
-	private Vertex<T> vertex2;
-	private int weight;
-	
-	Edge(Vertex<T> vertex1, Vertex<T> vertex2) {
-		this.vertex1 = vertex1;
-		this.vertex2 = vertex2;
+	Vertex (int val) {
+		this.val = val;
 	}
 	
-	Edge(Vertex<T> vertex1, Vertex<T> vertex2, boolean isDirected, int weight) {
-		this.vertex1 = vertex1;
-		this.vertex2 = vertex2;
-		this.isDirected = isDirected;
+	void setKey(int key) {
+		k = key;
+	}
+	
+	int getKey() {
+		return k;
+	}
+}
+
+class Edge<E> {
+	Vertex<E> v1;
+	Vertex<E> v2;
+	int weight;
+
+	Edge() {
+		v1 = null;
+		v2 = null;
+		weight = 0;
+	}
+
+	Edge(Vertex<E> v1, Vertex<E> v2, int weight) {
+		this.v1 = v1;
+		this.v2 = v2;
 		this.weight = weight;
 	}
-	
-	Edge(Vertex<T> vertex1, Vertex<T> vertex2, boolean isDirected) {
-		this.vertex1 = vertex1;
-		this.vertex2 = vertex2;
-		this.isDirected = isDirected;
-	}
-	
-	/*
-	 * Return edge weight
-	 */
+
 	int getWeight() {
 		return weight;
 	}
-	
-	Vertex<T> getVertex1() {
-		return vertex1;
+
+	Vertex<E> getVertex1() {
+		return v1;
 	}
-	
-	Vertex<T> getVertex2() {
-		return vertex2;
-	}
-	
-	boolean isDirected() {
-		return isDirected;
+
+	Vertex<E> getVertex2() {
+		return v2;
 	}
 }
